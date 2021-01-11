0a00fe2c0c867a899bb7f90e281239f116cf609b
Manfred Parbatia <superflyman90@gmail.com>
2021-01-11 13:12:25 +0100

    Menu Responsive
    Responsivization of the menu

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// var items = document.querySelector('.menu-item');
+// var header = document.querySelector('header');
+// var menu = document.querySelector('.menu');
+
+// console.log(items);
+
+// if(window.matchMedia("(min-width:800px)").matches){
+//    items.classList.replace("menu-item",'menu-items-link');
+//    header.style.height = "auto";
+// }
